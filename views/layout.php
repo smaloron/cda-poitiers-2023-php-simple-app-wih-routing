c36f3e844a824fe6333a9cbfa251490119a5e5d4
seb <contact@smaloron.eu>
2023-01-27 10:56:28 +0100

    Gestion des ressources externes

--- views/layout.php
+++ views/layout.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mon super site</title>
+    <link rel="stylesheet" href="/css/sakura/sakura-dark.css">
 </head>
 
 <body>
