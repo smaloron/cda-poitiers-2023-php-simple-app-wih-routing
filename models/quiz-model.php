71a39340b75c331f585abaa7261ae0ca14e6885b
seb <contact@smaloron.eu>
2023-01-30 10:31:38 +0100

    quiz, modification du modele pour gérer la mise à jour

--- models/quiz-model.php
+++ models/quiz-model.php
@@ -30,3 +30,58 @@ function addQuestion($question)
     $data["questions"][] = $question;
     file_put_contents(QUIZ_PATH, json_encode($data));
 }
+
+/**
+ * Recherche un index dans un tableau ordinal 
+ * en fonction d'un critère déterminé par la fonction
+ * callback passée en argument
+ *
+ * @param array $source le tableau dans leque on cherche
+ * @param callable $callback la fonction callback 
+ *                           qui doit retourner un booléen
+ * @return integer
+ */
+function findIndex(array $source, callable $callback): int
+{
+    $foundIndex = -1;
+    $arraySize = count($source);
+    for ($i = 0; $i < $arraySize; $i++) {
+        if ($callback($source[$i])) {
+            $foundIndex = $i;
+            break;
+        }
+    }
+
+    return $foundIndex;
+}
+/**
+ * Mise à jour d'une question
+ *
+ * @param array $quiz les données du quiz 
+ *              comprenant l'ensemble des questions
+ * @param array $question les nouvelles données de la question
+ * @return void
+ */
+function updateQuestion(array $quiz, array $question)
+{
+    // Recherche de l'index de la question à modifier
+    $index = findIndex($quiz["questions"], function ($item) use ($question) {
+        return $item["id"] == $question["id"];
+    });
+
+    // Modification et sauvegarde de la question
+    if ($index >= 0) {
+        $quiz["questions"][$index] = $question;
+        file_put_contents(QUIZ_PATH, json_encode($quiz));
+    }
+}
+
+function findOneQuestionById(int $id, array $quiz): array
+{
+    return array_filter(
+        $quiz["questions"],
+        function ($item) use ($id) {
+            return $item["id"] == $id;
+        }
+    );
+}
