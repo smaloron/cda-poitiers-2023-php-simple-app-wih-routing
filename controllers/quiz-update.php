81445d4a6b981021f89e53e3fb024ef60c258531
seb <contact@smaloron.eu>
2023-01-30 11:40:54 +0100

    quiz: traitement de la mise à jour

--- controllers/quiz-update.php
+++ controllers/quiz-update.php
@@ -6,6 +6,18 @@ $id = filter_input(INPUT_GET, "id", FILTER_DEFAULT);
 $quiz = findAll();
 $question = findOneQuestionById($id, $quiz);
 
+$isPosted = filter_has_var(INPUT_POST, "submit");
+if ($isPosted) {
+    $question = $_POST;
+    unset($question["submit"]);
+    // TODO : gérer le cas où on ajoute une nouvelle réponse et le cas ou l'on a supprimé une réponse
+    $question["id"] = $id;
+
+    updateQuestion(findAll(), $question);
+
+    header("location:/quiz-admin");
+}
+
 render(
     "quiz-form",
     [
