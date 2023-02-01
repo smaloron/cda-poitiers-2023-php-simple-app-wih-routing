18010a3c4e92cb9f93680823ad80a28105ba95e5
seb <contact@smaloron.eu>
2023-02-01 15:11:51 +0100

    crud adresse : affichage de la liste

---
+++ controllers/address/list.php
@@ -0,0 +1,10 @@
+<?php
+
+// faire une requête pour récupérer la liste des adresses
+
+// Afficher une vue pour présenter cette liste dans une table html
+
+/*
+rue | code_postal | ville | Actions
+aa  | aaaaaaa     | aaaaa | supprimer modifier
+*/
