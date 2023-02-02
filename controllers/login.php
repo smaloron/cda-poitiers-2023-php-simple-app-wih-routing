b5e9677af3520118e1f598f19996a4b5bee39920
seb <contact@smaloron.eu>
2023-02-02 16:12:23 +0100

    Sécurité : login

--- controllers/login.php
+++ controllers/login.php
@@ -1,10 +1,23 @@
 <?php
 require MODEL_DIR . "/user-model.php";
 
+$message = "";
+
+
 // Test pour savoir les les données ont été postées
+if (filter_has_var(INPUT_POST, "userName")) {
     // Si oui on récupère les données
+    $userName = filter_input(INPUT_POST, "userName", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $pass = filter_input(INPUT_POST, "userPassword", FILTER_DEFAULT);
     // appel à la fonction authenticate
-    // Si true : redirection vers home
-    // Sinon message d'erreur
+    if (authenticate($userName, $pass)) {
+        // Si true : redirection vers home
+        header("location:/home");
+    } else {
+        // Sinon message d'erreur
+        $message = "Vos infos de connexion sont incorrectes";
+    }
+}
 
 // Affichage de la vue en transmettant l'éventuel message d'erreur
+render("register", ["message" => $message]);
--- models/user-model.php
+++ models/user-model.php
@@ -19,9 +19,16 @@ function authenticate(string $userName, string $pass): bool
 {
     // Trouver un utilisateur qui possède le userName passé en argument
     // si aucun utilisateur trouvé on retourne faux
+    $pdo = getPDO();
+    $statement = $pdo->prepare("SELECT * FROM users WHERE user_name=?");
+    $statement->execute([$userName]);
+    $user = $statement->fetch();
 
-    // comparer les mots de passe avec la fonction password_verify
-    // retourne le résultat de la vérification
-
-
+    if ($user) {
+        // comparer les mots de passe avec la fonction password_verify
+        // retourne le résultat de la vérification
+        return password_verify($pass, $user["user_password"]);
+    } else {
+        return false;
+    }
 }
