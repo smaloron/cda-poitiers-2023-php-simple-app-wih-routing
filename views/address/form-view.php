09615af7095630c33a04ff22fb81763f101105e5
seb <contact@smaloron.eu>
2023-02-03 11:21:25 +0100

    requête ajax pour obtenr la liste des villes

--- views/address/form-view.php
+++ views/address/form-view.php
@@ -6,11 +6,12 @@
     </div>
     <div>
         <label>code postal</label>
-        <input type="text" name="code_postal" value="<?= $address["code_postal"] ?? "" ?>">
+        <input type="text" name="code_postal" value="<?= $address["code_postal"] ?? "" ?>" id="codePostal">
     </div>
     <div>
         <label>ville</label>
-        <input type="text" name="ville" value="<?= $address["ville"] ?? "" ?>">
+        <select type="text" name="ville" value="<?= $address["ville"] ?? "" ?>" id="selectVille">
+        </select>
     </div>
 
     <div>
@@ -19,3 +20,36 @@
         </button>
     </div>
 </form>
+
+<script>
+const $codePostal = document.getElementById("codePostal");
+const $ville = document.getElementById("selectVille");
+let cp;
+<?php if (isset($address["code_postal"])) : ?>
+ville = "<?= $address["ville"] ?>";
+<?php endif ?>
+
+// Récupération de la liste des villes depuis l'API
+async function getOptionsForSelect(codePostal) {
+    const response = await fetch('http://localhost:8000/api/cp-ville?cp=' + codePostal);
+    const data = await response.json();
+
+    if (response.status == '404') {
+        return "";
+    }
+    return data.map((item) => {
+        const selected = item == ville ? 'selected' : '';
+        return `<option ${selected}>${item}</option>`
+    }).join('');
+}
+
+// Modification de la liste des villes lors du changement de code postal
+$codePostal.onblur = (ev) => {
+    getOptionsForSelect(ev.target.value).then((data) => $ville.innerHTML = data);
+}
+
+<?php
+    if (isset($address["code_postal"])) : ?>
+getOptionsForSelect("<?= $address['code_postal'] ?>").then((data) => $ville.innerHTML = data);
+<?php endif; ?>
+</script>
